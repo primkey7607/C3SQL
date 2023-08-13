cf982398df035412ebbc1a4142537dc0022b78e1
primkey7607 <psubramaniam@uchicago.edu>
2023-08-13 13:13:02 -0500

    bug

--- scripts/gen_allprompts.py
+++ scripts/gen_allprompts.py
@@ -5,11 +5,11 @@ def generate_allprompts(indir, inprefix, outprefix):
         if f.startswith(inprefix) and f.endswith('.json'):
             suffix = f[len(inprefix):]
             fullf = os.path.join(indir, f)
-            cmd = "python prompt_generate.py --input_dataset_path " + fullf
+            cmd = "python ../src/prompt_generate.py --input_dataset_path " + fullf
             cmd += " --output_dataset_path " + outprefix + suffix
             os.system(cmd)
 
 if __name__=='__main__':
-    generate_allprompts('scripts', 'testgen', 'testtemp')
+    generate_allprompts('.', 'testgen', 'testtemp')
 
 
