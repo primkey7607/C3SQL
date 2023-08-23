3cf3a85f8ef7839fa42c87432251e27e22fed01e
primkey7607 <psubramaniam@uchicago.edu>
2023-08-22 23:44:40 -0500

    constructed tpch

--- scripts/construct_tpch.py
+++ scripts/construct_tpch.py
@@ -79,7 +79,7 @@ def gen_questions_from_docs(doc_dir, table_dir, outname):
                 doc_st = fh.read()
             doc = nlp(doc_st)
             for i,sent in enumerate(doc.sents):
-                get_tabcols(table_dir, sent, outname + '_doc' + str(doc_cnt) + '_sent' + str(i))
+                get_tabcols(table_dir, sent.text, outname + '_doc' + str(doc_cnt) + '_sent' + str(i))
             
 
 if __name__=='__main__':
