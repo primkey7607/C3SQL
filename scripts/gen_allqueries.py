cfad84c249a970acecf048b03fc98bc3755918d4
primkey7607 <psubramaniam@uchicago.edu>
2023-08-23 22:50:35 -0500

    one at a time

--- scripts/gen_allqueries.py
+++ scripts/gen_allqueries.py
@@ -6,6 +6,10 @@ def generate_allsql(indir, inprefix, outprefix):
     for f in os.listdir(indir):
         if f.startswith(inprefix) and f.endswith('.json'):
             suffix = f[len(inprefix):-5]
+            outfile = outprefix + suffix + '.sql'
+            if os.path.exists(outfile):
+                continue
+            
             fullf = os.path.join(indir, f)
             cmd = "python ../src/generate_sqls_by_gpt3.5.py --input_dataset_path " + fullf
             cmd += " --output_dataset_path " + outprefix + suffix + '.sql'
@@ -14,9 +18,9 @@ def generate_allsql(indir, inprefix, outprefix):
 if __name__=='__main__':
     # generate_allsql('../', 'testtemp', '../testsql')
     generate_allsql('../q1exp', 'q1exp', '../q1sql')
-    generate_allsql('../q2exp', 'q2exp', '../q2sql')
-    generate_allsql('../q3exp', 'q3exp', '../q3sql')
-    generate_allsql('../q4exp', 'q4exp', '../q4sql')
-    generate_allsql('../q5exp', 'q5exp', '../q5sql')
-    generate_allsql('../q6exp', 'q6exp', '../q6sql')
+    # generate_allsql('../q2exp', 'q2exp', '../q2sql')
+    # generate_allsql('../q3exp', 'q3exp', '../q3sql')
+    # generate_allsql('../q4exp', 'q4exp', '../q4sql')
+    # generate_allsql('../q5exp', 'q5exp', '../q5sql')
+    # generate_allsql('../q6exp', 'q6exp', '../q6sql')
 
