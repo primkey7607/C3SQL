5614d93238fcefce9c042e7c237e6c8b8f709a3a
primkey7607 <psubramaniam@uchicago.edu>
2023-08-23 19:46:25 -0500

    fixed efficiency problem

--- scripts/construct_tpch.py
+++ scripts/construct_tpch.py
@@ -61,6 +61,26 @@ def get_tabcols(dirname, statement, outname):
     with open(outname + '.json', 'w+') as fh:
         json.dump(out_obj, fh, indent=2)
 
+#efficient version of getting tabcols
+def get_tabcols_eff(tab_dfs, statement, outname):
+    out_dct = {}
+    out_dct['schema'] = {}
+    out_dct['db_contents'] = {}
+    out_dct['question'] = statement
+    out_dct['fk'] = []
+    for tname in tab_dfs:
+        df = tab_dfs[tname]
+        cols = df.columns.tolist()
+        out_dct['schema'][tname] = cols
+        out_dct['db_contents'][tname] = {}
+        for i,c in enumerate(cols):
+            out_dct['db_contents'][tname][int(i)] = None
+
+    out_obj = [out_dct]
+
+    with open(outname + '.json', 'w+') as fh:
+        json.dump(out_obj, fh, indent=2)
+
 def gen_questions(dirname, outname, roles, tables, privileges):
     cnt = 1
     for r in roles:
@@ -73,26 +93,35 @@ def gen_questions(dirname, outname, roles, tables, privileges):
 def gen_questions_from_docs(doc_dir, table_dir, outname):
     nlp = spacy.load("en_core_web_sm")
     doc_cnt = 0
+    tab_dfs = {}
+    for f in os.listdir(table_dir):
+        if f.endswith('.csv'):
+            tname = f[:-4]
+            df = pd.read_csv(os.path.join(table_dir, f))
+            tab_dfs[tname] = df
+            
     for f in os.listdir(doc_dir):
         if f.endswith('.txt'):
             with open(os.path.join(doc_dir, f)) as fh:
                 doc_st = fh.read()
             doc = nlp(doc_st)
             for i,sent in enumerate(doc.sents):
-                get_tabcols(table_dir, sent.text, outname + '_doc' + str(doc_cnt) + '_sent' + str(i))
+                if os.path.exists(outname + '_doc' + str(doc_cnt) + '_sent' + str(i) + '.json'):
+                    continue
+                get_tabcols_eff(tab_dfs, sent.text, outname + '_doc' + str(doc_cnt) + '_sent' + str(i))
             doc_cnt += 1
 
 if __name__=='__main__':
     csv_dir = os.path.expanduser('~/tpch-kit/scale1data/tpchcsvs')
     # random.seed(3)
     # gen_questions(csv_dir, 'testgen', random.sample(admin_lst, k=25), ['customer', 'lineitem'], ['SELECT', 'INSERT'])
-    gen_questions_from_docs('../../automatedgov/q1readonly/docs', csv_dir, '../q1exp/q1readonly')
-    gen_questions_from_docs('../../automatedgov/q2readwrite/docs', csv_dir, '../q2exp/q2readwrite')
-    gen_questions_from_docs('../../automatedgov/q3complex/docs', csv_dir, '../q3exp/q3complex')
-    gen_questions_from_docs('../../automatedgov/q4dac/docs', csv_dir, '../q4exp/q4dac')
+    # gen_questions_from_docs('../../automatedgov/q1readonly/docs', csv_dir, '../q1exp/q1readonly')
+    # gen_questions_from_docs('../../automatedgov/q2readwrite/docs', csv_dir, '../q2exp/q2readwrite')
+    # gen_questions_from_docs('../../automatedgov/q3complex/docs', csv_dir, '../q3exp/q3complex')
+    # gen_questions_from_docs('../../automatedgov/q4dac/docs', csv_dir, '../q4exp/q4dac')
     gen_questions_from_docs('../../automatedgov/q5complexview/docs', csv_dir, '../q5exp/q5complexview')
-    gen_questions_from_docs('../../automatedgov/q6dacview/docs', csv_dir, '../q6exp/q6dacview')
-            
+    # gen_questions_from_docs('../../automatedgov/q6dacview/docs', csv_dir, '../q6exp/q6dacview')
+    # gen_questions_from_docs('../../automatedgov/q6test', csv_dir, 'q6test')
             
             
             
